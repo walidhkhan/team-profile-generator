e22d9d671bff23bafd39c51677cedd181d19875d
walidhkhan <walidhkhan@gmail.com>
2021-08-22 18:16:38 -0700

    setting up jest debugging

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,5 +1,5 @@
-const Employee = require("../lib/Employee");
-​
+const Employee = require("../lib/Employee.js");
+
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
   expect(typeof(e)).toBe("object");
